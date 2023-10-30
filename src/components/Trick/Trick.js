99aecf8e612ca7103bc0d740d46a08ff7b8ceb26
Johann Dee <johanndee1994@gmail.com>
2023-10-30 09:08:11 -0700

    Add styling to Trick

--- src/components/Trick/Trick.js
+++ src/components/Trick/Trick.js
@@ -3,9 +3,10 @@ import './Trick.css'
 function Trick({ id, stance, name, obstacle, tutorial}) {
   return (
     <div className="trick">
-      <p>{stance} {name}</p>
-      <p>Obstacle: {obstacle}</p>
-      <p>Link to Tutorial: {tutorial}</p>
+      <p>{stance.charAt(0).toUpperCase() + stance.slice(1)} {name.charAt(0).toUpperCase() + name.slice(1)}</p>
+      <p>Obstacle: {obstacle.charAt(0).toUpperCase() + obstacle.slice(1)}</p>
+      <p>Link to Tutorial:</p>
+      <a href={tutorial}>{tutorial}</a>
     </div>
   )
 }
