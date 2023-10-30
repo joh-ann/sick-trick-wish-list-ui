31abfecf9dd3b2146774e2a5a21d216f77024154
Johann Dee <johanndee1994@gmail.com>
2023-10-30 08:39:24 -0700

    Fetch all tricks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useState, setState } from 'react'
 
 
 function App() {
+
+  function getAllTricks() {
+    return fetch("http://localhost:3001/api/v1/tricks")
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+  useEffect(() => {
+    getAllTricks();
+  })
+  
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
